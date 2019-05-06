0e842b1ce28126a83a843c61bd5a4383d61654fa
Dominick Del Bosque <dominick@theosfp.com>
2019-05-05 20:10:35 -0700

    Updated environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,20 +1,8 @@
-# ENV['SINATRA_ENV'] ||= "development"
-# ENV['RACK_ENV'] ||= "development"
+ENV['SINATRA_ENV'] ||= "development"
+ENV['RACK_ENV'] ||= "development"
 
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
-# require 'require_all'
-# require_all 'app'
-
-configure :production, :development do
-db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/bucket_dev')
-ActiveRecord::Base.establish_connection(
-  adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  host: db.host,
-  username: db.user,
-  password: db.password,
-  database: db.path[1..-1],
-  encoding: 'utf8'
-)
-end
+require 'require_all'
+require_all 'app'
