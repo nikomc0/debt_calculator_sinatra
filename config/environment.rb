365d39eb932128ea1235d3bc56fffba1cef517d4
Dominick Del Bosque <dominick@theosfp.com>
2019-05-05 19:55:49 -0700

    Updated environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,3 @@
-require 'bundler/setup'
-require 'require_all'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-require_all 'app'
-
 configure :development do
 	ENV['SINATRA_ENV'] ||= "development"
 	ENV['RACK_ENV'] ||= "development"
@@ -11,3 +5,14 @@ configure :development do
 	require 'bundler/setup'
 	Bundler.require(:default, ENV['SINATRA_ENV'])
 end
+
+configure :production do
+	ENV['SINATRA_ENV'] ||= "production"
+	ENV['RACK_ENV'] ||= "production"
+
+	require 'bundler/setup'
+	Bundler.require(:default, ENV['SINATRA_ENV'])
+end
+
+require 'require_all'
+require_all 'app'
