b40f7b1e596c68f59a6194647cc87c8034b643e3
Dominick Del Bosque <dominick@theosfp.com>
2019-10-07 06:15:06 -0700

    Add error handling for not found payments.
    This will prevent vague internal error messages in production.
    Instead an error message showing "Payment/Account not found."
    
     - Added rescue block to PATCH payments in account_controller.

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -102,6 +102,10 @@ class AccountsController < ApplicationController
   	@payment.destroy
   	flash[:success] = "Payment has been saved."
   	redirect "/accounts/#{params[:account_id]}"
+
+  rescue ActiveRecord::RecordNotFound => e
+		flash[:danger] = "Account does not exist."
+		redirect back
   end
 
 	delete '/accounts/:id' do 
